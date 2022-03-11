f9eed1fb1fb4f3010ec3628f9ddf51dd1e71b5ee
jareddodson <jared.dodson04@gmail.com>
2022-03-11 11:37:51 -0600

    created functions for all operations

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+}
+
+clear() {
+
+}
+
+delete() {
+
+}
+
+appendNumber(number) {
+
+}
+
+chooseOperation(operation) {
+
+}
+
+compute() {
+
+}
+
+updateDisplay() {
+    
+}
+
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operations]');
 const equalButton = document.querySelector('[data-equals]');
