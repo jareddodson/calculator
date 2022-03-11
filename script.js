7aced17d634dd560be859e97838390b65dbb8747
jareddodson <jared.dodson04@gmail.com>
2022-03-11 12:16:02 -0600

    finished compute function

--- script.js
+++ script.js
@@ -32,7 +32,29 @@ chooseOperation(operation) {
 }
 
 compute() {
-
+    let computation
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+    if (isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+        case '+':
+            computation = prev + current
+            break
+        case '-':
+            computation = prev - current
+            break
+        case '*':
+            computation = prev * current
+            break
+        case '/':
+            computation = prev / current
+            break
+        default:
+            return
+    }
+    this.currentOperand = computation;
+    this.operation = undefined;
+    this.previousOperand = '';
 }
 
 updateDisplay() {
@@ -51,6 +73,11 @@ operationButtons.forEach(button => {
         calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
     })
+})
+
+equalButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
 })
 
 const numberButtons = document.querySelectorAll('[data-number]');
